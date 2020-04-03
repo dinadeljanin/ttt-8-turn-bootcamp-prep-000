2647cb8a4570e258711cce5c0cf7101370214f46
D D <ddeljanin5@icloud.com>
2020-04-03 04:36:40 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -37,7 +37,7 @@ def turn(board)
   input = gets.chomp
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, char)
+    move(board, index, 'X')
     display_board(board)
   else
     turn(board)
