3c948fe354632c7b3d772f8d33f66f91153f4f6c
D D <ddeljanin5@icloud.com>
2020-04-03 04:36:26 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,17 +32,17 @@ def move(board, index, char = 'X')
   board[index] = char
 end
 
-# def turn(board)
-#   puts "Please enter 1-9:"
-#   input = gets.chomp
-#   index = input_to_index(input)
-#   if valid_move?(board, index)
-#     move(board, index, char)
-#     display_board(board)
-#   else
-#     turn(board)
-#   end
-# end
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.chomp
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, char)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
 
 def turn(board)
   puts "Please enter 1-9:"
