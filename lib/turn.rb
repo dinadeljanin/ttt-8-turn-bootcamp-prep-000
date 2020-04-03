a91410938f213b588d8bf7a58666f8adb98147a7
D D <ddeljanin5@icloud.com>
2020-04-03 04:31:36 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,7 +28,7 @@ def input_to_index(input)
   index = input.to_i - 1 # convert input to index
 end
 
-def move(board, index, token = "X")
+def move(board, index, char = "X")
   board[input] = 'X'
 end
 
